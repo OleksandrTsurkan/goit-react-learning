cda0d708b6e2b9aa16d672fcb2fd82930ce1ea59
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-09-29 12:36:04 +0300

    2990

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,84 +1,83 @@
-import { Component, useEffect, useState } from 'react'
-import Todo from '../Todo'
+import { useEffect, useState } from "react";
+import Todo from "../Todo";
 
-import { getAllTodo } from '../../api/todo'
-import FormCreateTodo from '../Forms/FormCreateTodo'
-import FormFilterTodo from '../Forms/FormFilterTodo'
-import todoData from '../../data.json'
-import { nanoid } from 'nanoid'
+import FormCreateTodo from "../Forms/FormCreateTodo";
+import FormFilterTodo from "../Forms/FormFilterTodo";
+import todoData from "../../data.json";
+import { nanoid } from "nanoid";
 
 const TodoList = () => {
-	const [todo, setTodo] = useState(null)
-	const [filteredTodo, setFilteredTodo] = useState(null)
+  const [todo, setTodo] = useState(null);
+  const [filteredTodo, setFilteredTodo] = useState(null);
 
-	useEffect(() => {
-		const localData = localStorage.getItem('todo')
-		localData && JSON.parse(localData).length
-			? setTodo(JSON.parse(localData))
-			: setTodo(todoData)
-	}, [])
+  useEffect(() => {
+    const localData = localStorage.getItem("todo");
+    localData && JSON.parse(localData).length
+      ? setTodo(JSON.parse(localData))
+      : setTodo(todoData);
+  }, []);
 
-	useEffect(() => {
-		todo && localStorage.setItem('todo', JSON.stringify(todo))
-	}, [todo])
+  useEffect(() => {
+    todo && localStorage.setItem("todo", JSON.stringify(todo));
+  }, [todo]);
 
-	const handleClick = () => {
-		this.setState({ isShowTodos: true })
-	}
+  const handleClick = () => {
+    this.setState({ isShowTodos: true });
+  };
 
-	const handleDelete = (id) => {
-		setTodo((prev) => prev.filter((el) => el.id !== id))
-	}
+  const handleDelete = (id) => {
+    setTodo((prev) => prev.filter((el) => el.id !== id));
+  };
 
-	const createTodo = (dataByForm) => {
-		const newTodo = {
-			...dataByForm,
-			id: nanoid(),
-			completed: false,
-		}
-		setTodo((prev) => [...prev, newTodo])
-	}
+  const createTodo = (dataByForm) => {
+    const newTodo = {
+      ...dataByForm,
+      id: nanoid(),
+      completed: false,
+    };
+    setTodo((prev) => [...prev, newTodo]);
+  };
 
-	const filterTodo = (filterQuery) => {
-		setFilteredTodo(
-			todo.filter((el) =>
-				el.title.toLowerCase().includes(filterQuery.toLowerCase())
-			)
-		)
-	}
+  const filterTodo = (filterQuery) => {
+    setFilteredTodo(
+      todo.filter((el) =>
+        el.title.toLowerCase().includes(filterQuery.toLowerCase())
+      )
+    );
+  };
 
-	const handleCheck = (id) => {
-		setTodo((prev) =>
-			prev.map((el) =>
-				el.id === id ? { ...el, completed: !el.completed } : el
-			)
-		)
-	}
+  const handleCheck = (id) => {
+    setTodo((prev) =>
+      prev.map((el) =>
+        el.id === id ? { ...el, completed: !el.completed } : el
+      )
+    );
+  };
 
-	return (
-		<div className='container'>
-			<button className='btn btn-success' onClick={handleClick}>
-				Show all todo's
-			</button>
-			<FormCreateTodo createTodo={createTodo} />
-			<FormFilterTodo filterTodo={filterTodo} />
-			{todo && (
-				<ul className='list-group'>
-					{(filteredTodo ?? todo).map((el) => (
-						<Todo
-							todo={el}
-							key={el.id}
-							handleDelete={handleDelete}
-							handleCheck={handleCheck}
-						/>
-					))}
-				</ul>
-			)}
-		</div>
-	)
-}
+  return (
+    <div className="container">
+      <button className="btn btn-success" onClick={handleClick}>
+        Show all todo's
+      </button>
+      <FormCreateTodo createTodo={createTodo} />
+      <FormFilterTodo filterTodo={filterTodo} />
+      {todo && (
+        <ul className="list-group">
+          {(filteredTodo ?? todo).map((el) => (
+            <Todo
+              todo={el}
+              key={el.id}
+              handleDelete={handleDelete}
+              handleCheck={handleCheck}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
 
 // class TodoList extends Component {
 // 	state = {
